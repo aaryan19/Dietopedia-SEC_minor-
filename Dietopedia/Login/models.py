8640c34275f84f70fc557dc06c8e2f0a6a3ccd67
ShaurabhT <47441444+ShaurabhT@users.noreply.github.com>
2020-10-09 10:47:49 +0545

    Test Database

--- Dietopedia/Login/models.py
+++ Dietopedia/Login/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 class Customers(models.Model):
     First_Name= models.CharField(max_length=20)
     Last_Name=models.CharField(max_length=20)
-    Gender=models.CharField(max_length=10)
+    Gender=models.CharField(max_length=20)
     Age=models.IntegerField()
     Email= models.EmailField(max_length=70,blank=True, null= True, unique= True)
 
